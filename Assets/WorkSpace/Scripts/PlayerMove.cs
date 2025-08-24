7b4807568db265a86f89647428cba1367a8ae07e
shuto20051014 <shuto20051014@gmail.com>
2025-08-25 04:44:10 +0900

    [add]プレイヤーの動き、弾、壁の追加

---
+++ Assets/WorkSpace/Scripts/PlayerMove.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] public float _moveSpeed = 10;
+    [SerializeField] private Transform _muzzle;
+    [SerializeField] private GameObject _BulletPrefab;
+    [SerializeField] private float _BulletSpeed = 10;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 velocity = new Vector2(h, v).normalized;
+
+        _rigidbody2D.velocity = velocity * _moveSpeed;
+    }
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(_BulletPrefab, _muzzle.position, Quaternion.identity);
+        if (bullet.TryGetComponent(out Rigidbody2D rigidBody2D))
+        {
+            Vector2 velocity = _muzzle.up * _BulletSpeed; rigidBody2D.velocity = velocity;
+        }
+    }
+}
