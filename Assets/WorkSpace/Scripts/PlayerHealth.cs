5e41e9c364ec9a849e775dcaa9282a69a03590e5
shuto20051014 <shuto20051014@gmail.com>
2025-08-29 07:22:27 +0900

    [add]ゲームオーバーのフラグの追加

--- Assets/WorkSpace/Scripts/PlayerHealth.cs
+++ Assets/WorkSpace/Scripts/PlayerHealth.cs
@@ -1,28 +1,36 @@
-using UnityEngine.UI;
 using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] Image _image;
+    [Header("HP")]
     [SerializeField] float _maxHP = 100;
     [SerializeField] float _currentHP = 100;
+    [SerializeField] RectTransform _PlayerHpBar;
+    float _Width = 100;
+    [SerializeField] public bool _isdead = false;
     void Start()
     {
         //HP‚Ì‰Šú‰»
         _currentHP = _maxHP;
+        UpdateHpBar();
     }
     public void EnemytoPlayerDamage(int _damage)
     {
         //0‚ğ‰º‰ñ‚ç‚È‚¢‚æ‚¤‚É‚·‚é
         _currentHP = Mathf.Max(_currentHP - _damage, 0);
-        //fillAmount‚É‘ã“ü
-        _image.fillAmount = _currentHP / _maxHP;
-        if(_currentHP <= 0)
+        UpdateHpBar();
+        if (_currentHP <= 0)
         {
             Gameover();
         }
     }
+    void UpdateHpBar()
+    {
+        float ratio = _currentHP / _maxHP;
+        _PlayerHpBar.sizeDelta = new Vector2(_Width * ratio, _PlayerHpBar.sizeDelta.y);
+    }
     void Gameover()
     {
+        _isdead = true;
         Debug.Log("Gameover");
     }
     //void Heal()
